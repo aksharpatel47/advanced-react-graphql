447cb3be98af9609cf14e38cec3e553dfd4f6d3b
Akshar Patel <akshar.patel.47@gmail.com>
2019-02-07 00:54:54 -0700

    feat: setup frontend code

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default () => (
+  <div>
+    <h1>Hi!</h1>
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+  </div>
+);
---
+++ frontend/pages/sell.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default () => (
+  <div>
+    <h1>Sell</h1>
+    <Link href="/">
+      <a>Home</a>
+    </Link>
+  </div>
+);
