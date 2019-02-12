d14cfef902cb87eb20938b23c4969b3e6aba82a0
Akshar Patel <akshar.patel.47@gmail.com>
2019-02-11 23:06:26 -0700

    feat: complete module 1 of course

---
+++ frontend/components/Meta.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export const Meta = () => (
+  <Head>
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <meta charSet="utf-8" />
+    <link rel="stylesheet" href="/static/nprogress.css" type="text/css" />
+    <link rel="shortcut icon" href="/static/favicon.png" />
+    <title>Sick Fits!</title>
+  </Head>
+);
